c19019de9f4e7eb4cd862162474e024967c5106d
A-l-e-x-B-o-y <Uncle.El.Alex@gmail.com>
2022-06-27 08:40:02 +0300

    Test

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
@@ -0,0 +1,43 @@
+package com.javarush.task.pro.task14.task1415;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Стек в домашних условиях
+*/
+
+public class MyStack {
+
+    private final List<String> storage = new ArrayList<>();
+
+
+    public void push(String s) {
+        storage.add(0, s);
+    }
+
+    public String pop() {
+        String s = storage.get(0);
+        storage.remove(0);
+        return s;
+    }
+
+    public String peek() {
+        String s = storage.get(0);
+        return s;
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        int index = -1;
+        for (int i = 0; i < storage.size(); i++) {
+            if (storage.get(i).equals(s)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+}
